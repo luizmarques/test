5387be5d734b8b8138f34d3510550ab5c08f7d1e
LINIQUER SILVA <liniquer.silva@prof.infnet.edu.br>
2021-10-26 08:37:58 -0300

    footer implementation with styles

---
+++ src/components/Layout/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+import Footer from "./Footer";
+import Header from "./Header";
+
+export const Layout = ({ children, startTransparent = false }) => {
+  return (
+    <React.Fragment>
+      <Header />
+      <WrapStyled $startTransparent={startTransparent}>{children}</WrapStyled>
+      <Footer />
+    </React.Fragment>
+  );
+};
+
+const WrapStyled = styled.div`
+  ${(props) =>
+    !props.$startTransparent && `
+    padding: 77px;
+  `}
+`;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './config/startBootstrap'
 import Routers from "./routers";
+import GlobalStyled from "./config/GlobalStyled";
+
+document.title = "Fox Entregas";
 
 ReactDOM.render(
   <React.StrictMode>
     <Routers />
+    <GlobalStyled />
   </React.StrictMode>,
   document.getElementById("root")
 );
